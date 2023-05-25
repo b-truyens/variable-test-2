b5b12c969f3add3d4e785b70219bad73a3d2ab63
b-truyens <boristruyens@protonmail.com>
2023-05-25 02:51:56 +0200

    CRUD categories workin

---
+++ resources/views/cards.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+    <script src=""></script>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP2 - CVO Volt. Boris Truyens - app layout.
+    </title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <a href="" onclick="history.back()" class="button">
+                <i class="fa fa-arrow-left"></i> Terug
+            </a>
+            <h2>My first web app</h2>
+            <a href="./pages/settings.html" class="button">
+                <i class="fa fa-cog"></i>
+            </a>
+        </header>
+        <main>
+            <div class="container">
+              <!-- <div id="hero">
+                <div class="hero-text">
+                  <h1>Hallo wereld!</h1>
+                  <p>Dit is mijn eerste web app!</p>
+                </div>
+              </div> -->
+                <div class="band">
+                  <div class="item-XL">
+                    <a href="#" class="card">
+                      <div class="thumb" style="background-image: url(./img/Blur-2.png);"></div>
+                      <article>
+                        <h1>Suscipit praesentium ullam non architecto enim at!</h1>
+                        <p>Malesuada in justo rhoncus etiam aenean vel sed torquent tempus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+
+                        <p class="txt-right">Ex, omnis quos? Ipsum dolor sit amet, consectetur adipisicing elit. modi quas, nulla qui, dolorem quidem?</p>
+                        <button type="button" class="btn btn-primary btn-sm btn-right">Small button</button>
+ 
+                        <span class="r">Mary Winkler</span>
+                      </article>
+                    </a>
+                  </div>
+                  <div class="item-2">
+                    <a href="#" class="card">
+                      <div class="thumb" style="background-image: url(./img/blur-8.jpg);"></div>
+                      <article>
+                        <h1>Lobortis finibus  ultricies vitae lorem justo euismod urna.</h1>
+                        <p>Malesuada in justo rhoncus etiam aenean vel sed torquent tempus.</p>
+                        <button type="button" class="btn btn-secondary btn-sm btn-left">Small button</button>
+                        <span>Primis cursus</span>
+                      </article>
+                    </a>
+                  </div>
+                    <div class="item-1">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/Blur-1.png);"></div>
+                        <article>
+                          <h1>Diam nibh pellentesque quisque convallis magnis curae.</h1>
+                          <p>Molestie tempus accumsan suscipit faucibus eget consectetuer urna.</p>
+                          <span>Dellentesque.</span>
+                        </article>
+                      </a>
+                    </div>
+                    <div class="item-3">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/Blur-3.png);"></div>
+                        <article>
+                          <h1>Elementum consequat dictum porttitor</h1>
+                          <p>Molestie ut mattis senectus quam dictumst facilisis duis nunc ornare vel eu rhoncus curabitur scelerisque. Consectetuer. Sodales pharetra finibus. </p>
+                          <span>Melody Nieves</span>
+                        </article>
+                      </a>
+                    </div>
+                    <div class="item-4">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/blur-4.jpg);"></div>
+                        <article>
+                          <h1>Enim feugiat lacus vehicula nec sagittis duis.</h1>
+                          <p>Facilisis duis nunc ornare vel eu rhoncus curabitur scelerisque.</p>
+                          <span>Kezz Bracey</span>
+                        </article>
+                      </a>
+                    </div>
+                    <div class="item-5">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/blur-5.jpg);"></div>
+                        <article>
+                          <h1>Penatibus etiam molestie nullam ac potenti habitasse himenaeos vehicula.</h1>
+                          <p>Curae mauris commodo magnis a fusce interdum aliquam volutpat ligula.</p>
+                          <span>Rose</span>
+                        </article>
+                      </a>
+                    </div>
+                    <div class="item-6">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/blur-6.jpg);"></div>
+                        <article>
+                          <h1>Nisi posuere molestie  augue nulla convallis per primis.</h1>
+                          <p> Massa mi elementum pharetra posuere mus.</p>
+                          <span>Rhoncus Elit</span>
+                        </article>
+                      </a>
+                    </div>
+                    <div class="item-7">
+                      <a href="#" class="card">
+                        <div class="thumb" style="background-image: url(./img/blur-7.jpg);"></div>
+                        <article>
+                          <h1>Molestie ut mattis senectus.</h1>
+                          <p> quam dictumst consectetuer. Sodales pharetra elementum consequat dictum porttitor finibus</p>
+                          <span>Ut libero metus dapibus dis vehicula.</span>
+                        </article>
+                      </a>
+                    </div>
+            </div>
+        </main>
+        <nav>
+            <a href=" ./index.html " id="current">
+                <i class="fa fa-home "></i><div class="nav-txt">Home</div>
+            </a>
+            <a href="./pages/notifications.html ">
+                <i class="fa fa-bell "></i><div class="nav-txt">Notificaties</div>
+            </a>
+            <a href="./pages/messages.html ">
+                <i class="fa fa-envelope "></i><div class="nav-txt">Berichten</div>
+            </a>
+            <a href="./pages/about.html ">
+                <i class="fa fa-user "></i><div class="nav-txt">Profiel</div>
+            </a>
+        </nav>
+        </div>
+</body>
+
+ 
