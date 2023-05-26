74b5bf9f0602f3af09def98a4303fe581dc9d62a
b-truyens <boristruyens@protonmail.com>
2023-05-26 13:15:11 +0200

    import waterhole forum

---
+++ config/waterhole/design.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Theme
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to configure which theme Waterhole uses. A null
+    | value means that Waterhole will allow users to toggle between light
+    | and dark mode. You can disable this toggle by setting this to either
+    | "light" or "dark".
+    |
+    */
+
+    'theme' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twemoji Base URL
+    |--------------------------------------------------------------------------
+    |
+    | This option is the base URL from which to serve Twemoji assets. You can
+    | set this to null to disable Twemoji and fall back to system emoji.
+    |
+    */
+
+    'twemoji_base' => 'https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.1/',
+];
